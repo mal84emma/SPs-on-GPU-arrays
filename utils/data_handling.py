cc914329b6f4637f4225a46c99f3b2871b0cd8ba
mal84emma <mal84@cam.ac.uk>
2024-08-28 16:51:51 +0100

    Implement file IO for ScenarioData class

--- utils/data_handling.py
+++ utils/data_handling.py
@@ -3,6 +3,7 @@
 import os
 import csv
 import ast
+import yaml
 import numpy as np
 import pandas as pd
 
@@ -33,6 +34,78 @@ class ScenarioData:
         self.storage_efficiencies = [storage_dict[tech]['efficiency'] for tech in self.storage_technologies]
         self.discharge_ratios = [storage_dict[tech]['discharge_ratio'] for tech in self.storage_technologies]
 
+    def __str__(self):
+
+        ts_dict, cost_dict, storage_dict = self.to_file(None, save=False)
+
+        str = ''
+        str += 'Scenario Data\n'
+        str += '=============\n'
+
+        str += 'Time Series Data\n'
+        str += '----------------\n'
+        for key, val in ts_dict.items():
+            str += f'{key}: {val}\n'
+
+        str += '\nCost Data\n'
+        str += '---------\n'
+        for key, val in cost_dict.items():
+            str += f'{key}: {val}\n'
+
+        str += '\nStorage Data\n'
+        str += '------------\n'
+        for tech, data in storage_dict.items():
+            str += f'{tech}:\n'
+            for key, val in data.items():
+                str += f'  {key}: {val}\n'
+
+        return str
+
+    def to_file(self, fpath, save=True):
+        """Save scenario info to json."""
+
+        ts_dict = {
+            'load_level': self.load_level,
+            'wind_year': self.wind_year,
+            'solar_year': self.solar_year,
+            'price_year': self.price_year,
+            'carbon_year': self.carbon_year
+        }
+        cost_dict = {
+            'wind_capex': self.wind_capex,
+            'wind_opex': self.wind_opex,
+            'wind_lifetime': self.wind_lifetime,
+            'solar_capex': self.solar_capex,
+            'solar_opex': self.solar_opex,
+            'solar_lifetime': self.solar_lifetime,
+            'carbon_price': self.carbon_price
+        }
+        storage_dict = {
+            tech: {
+                'cost': self.storage_costs[i],
+                'lifetime': self.storage_lifetimes[i],
+                'efficiency': self.storage_efficiencies[i],
+                'discharge_ratio': self.discharge_ratios[i]
+            } for i, tech in enumerate(self.storage_technologies)
+        }
+
+        if save:
+            with open(fpath, 'w') as f:
+                yaml.dump({
+                    'ts_dict': ts_dict,
+                    'cost_dict': cost_dict,
+                    'storage_dict': storage_dict
+                }, f)
+
+        return ts_dict, cost_dict, storage_dict
+
+    def from_file(fpath):
+
+        with open(fpath, 'r') as f:
+            data = yaml.safe_load(f)
+
+        return ScenarioData(data['ts_dict'], data['cost_dict'], data['storage_dict'])
+
     def load_timeseries(self, dataset_dir):
         """Load timeseries data from CSV files."""
 
@@ -42,95 +115,6 @@ class ScenarioData:
         self.elec_prices = pd.read_csv(os.path.join(dataset_dir,'price',f'{self.price_year}.csv'))['Electricity price [EUR/kWh]'].to_numpy()
         self.carbon_intensity = pd.read_csv(os.path.join(dataset_dir,'carbon',f'{self.carbon_year}.csv'))['Carbon intensity [kgCO2/kWh]'].to_numpy()
 
-
-
-def save_scenarios(scenarios, measurements, out_path):
-    """Save sampled scenarios to CSV."""
-
-    header = ['Scenario no.']
-    header.extend([f'SB{i}' for i in range(scenarios.shape[1])])
-    header.extend([f'BM{i}' for i in range(measurements.shape[1])])
-
-    with open(out_path, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(header)
-        for scenario_no, scenario in enumerate(scenarios):
-            row = [scenario_no]
-            row += [format_scenario_tuple(tuple(bs)) for bs in scenario]
-            row += [format_scenario_tuple(tuple(bm)) for bm in measurements[scenario_no]]
-            writer.writerow(row)
-
-def load_scenarios(in_path):
-    """Load sampled scenarios from CSV."""
-
-    with open(in_path, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        header = next(reader)
-        n_buildings = (len(header) - 1) // 2
-
-        scenarios = []
-        measurements = []
-        for row in reader:
-            scenarios.append([ast.literal_eval(t) for t in row[1:n_buildings+1]])
-            measurements.append([ast.literal_eval(t) for t in row[n_buildings+1:]])
-
-    return np.array(scenarios), np.array(measurements)
-
-def save_design_results(results, out_path):
-    """Save LP design results & used scenarios to CSV."""
-
-    design_header = ['Parameter', 'Units']
-    design_header.extend([f'SB{i}' for i in range(results['reduced_scenarios'].shape[1])])
-    design_rows = [
-        ['Battery Capacity', 'kWh', *results['battery_capacities'].flatten()],
-        ['Solar Capacity', 'kWp', *results['solar_capacities'].flatten()],
-        ['Grid Con. Capacity', 'kW', results['grid_con_capacity']]
-    ]
-
-    objective_header = ['Objective Components', 'Value ($)']
-    obj_contr_labels = ['Total','Elec. Price','Carbon Cost','Grid Ex. Cost','Grid Cap. Cost','Battery Cost','Solar Cost']
-    obj_contrs = [results['objective'],*results['objective_contrs']]
-
-    scenarios_header = ['Scenario no.', 'Prob']
-    scenarios_header.extend([f'SB{i}' for i in range(results['reduced_scenarios'].shape[1])])
-
-    with open(out_path, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-
-        writer.writerow(['Design'])
-        writer.writerow(design_header)
-        for row in design_rows:
-            writer.writerow(row)
-
-        writer.writerow(['Objective'])
-        writer.writerow(objective_header)
-        for label,val in zip(obj_contr_labels,obj_contrs):
-            writer.writerow([label,val])
-
-        writer.writerow(['Reduced Scenarios'])
-        writer.writerow(scenarios_header)
-        for scenario_no, (scenario,prob) in enumerate(zip(results['reduced_scenarios'],results['reduced_probs'])):
-            writer.writerow([scenario_no] + [prob] + [format_scenario_tuple(tuple(bs)) for bs in scenario])
-
-def load_design_results(in_path):
-    """Load LP design results & used scenarios from CSV."""
-
-    results = {}
-    with open(in_path, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        rows = [row for row in reader]
-
-    # Load design results.
-    results['battery_capacities'] = np.array([float(t) for t in rows[2][2:]])[np.newaxis].T
-    results['solar_capacities'] = np.array([float(t) for t in rows[3][2:]])[np.newaxis].T
-    results['grid_con_capacity'] = float(rows[4][2])
-
-    # Load objective results.
-    results['objective'] = float(rows[7][1])
-    results['objective_contrs'] = np.array([float(t) for t in [row[1] for row in rows[8:14]]])
-
-    # Load reduced scenarios.
-    results['reduced_scenarios'] = np.array([[ast.literal_eval(t) for t in row[2:]] for row in rows[16:]])
-    results['reduced_probs'] = np.array([float(row[1]) for row in rows[16:]])
-
-    return results
+# TODO: implement saving of optimization results, including scenario data
+# may need to use deeper dir structure for this as scenarios are big and probably
+# need to be saved in separate files
