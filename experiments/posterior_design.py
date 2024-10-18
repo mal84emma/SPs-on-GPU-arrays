6f2d11c57cec9ebbc8e10ae43d488126ab625cdb
mal84emma <mal84@cam.ac.uk>
2024-10-18 10:20:34 +0100

    Avoid resolving restrictred design after shift to solving all tech combos independently (copy from precomputed solution)

--- experiments/posterior_design.py
+++ experiments/posterior_design.py
@@ -9,12 +9,12 @@ from tqdm import tqdm
 import multiprocess as mp
 from functools import partial
 
-from utils import ScenarioData, get_current_time, get_Gurobi_WLS_env, solve_model, try_all_designs
+from utils import ScenarioData, get_current_time, get_Gurobi_WLS_env, try_all_designs
 from configs import get_experiment_config
 
 
 
-def posterior_design(measured_scenario_id, settings, restricted_tech):
+def posterior_design(measured_scenario_id, settings):
 
     save_dir = os.path.join(*settings['results_dir'],'posterior',f'z_scenario_{measured_scenario_id}')
 
@@ -26,36 +26,18 @@ def posterior_design(measured_scenario_id, settings, restricted_tech):
     posterior_scenario_fpattern = os.path.join(posterior_scenarios_dir,f'z_scenario_{measured_scenario_id}','scenario_{j}.yaml')
     posterior_scenarios = [ScenarioData.from_file(posterior_scenario_fpattern.format(j=j)) for j in range(prob_settings['n_posterior_samples'])]
 
-    # Set up design restrictions
-    if not restricted_tech: # perform design with optionality (open)
-        design_name = 'open_design'
-        pass
+    print(f"Starting posterior design for scenario {measured_scenario_id} @ {get_current_time()}")
+    # Perform design (trying all tech combos) & save unrestricted design
+    best_model = try_all_designs(posterior_scenarios, settings, save_all=os.path.join(save_dir,'design_options'))
+    best_model.save_results(os.path.join(save_dir,f'open_design.yaml'))
 
-    elif restricted_tech: # perform design without optionality (restricted)
-        design_name = 'restricted_design'
+    # Save design with prior selected technologies as restricted design
+    with open(os.path.join(*settings['results_dir'],'prior','design.yaml'), 'r') as f: prior_design = yaml.safe_load(f)
+    prior_techs_str = '-'.join(prior_design['design']['storage_technologies'])
+    target_file = os.path.join(save_dir,'design_options',f'{prior_techs_str}_design.yaml')
+    shutil.copy(target_file, os.path.join(save_dir,f'restricted_design.yaml'))
 
-        # load storage technologies selected in prior design
-        with open(os.path.join(*settings['results_dir'],'prior','design.yaml'), 'r') as f: prior_design = yaml.safe_load(f)
-        settings['model_settings']['storage_technologies'] = prior_design['design']['storage_technologies'] # update model settings
-
-        # check if unrestricted design used prior selected technologies
-        open_design_file = os.path.join(save_dir,f'open_design.yaml')
-        if os.path.isfile(open_design_file):
-            with open(open_design_file, 'r') as f: open_design = yaml.safe_load(f)
-            if open_design['design']['storage_technologies'] == prior_design['design']['storage_technologies']:
-                # skip computation of restriction design, as same as open design
-                print(f"Skipping restricted design for scenario {measured_scenario_id} as open design used prior selected technologies")
-                shutil.copy(open_design_file, os.path.join(save_dir,f'restricted_design.yaml'))
-                return
-
-    # Perform design
-    print(f"Starting posterior {design_name} for scenario {measured_scenario_id} @ {get_current_time()}")
-    if not restricted_tech:
-        solved_model = try_all_designs(posterior_scenarios, settings)
-    elif restricted_tech:
-        solved_model = solve_model(posterior_scenarios, settings)
-    solved_model.save_results(os.path.join(save_dir,f'{design_name}.yaml'))
-    print(f"Finished posterior {design_name} for scenario {measured_scenario_id} @ {get_current_time()}")
+    print(f"Finished posterior design for scenario {measured_scenario_id} @ {get_current_time()}")
 
 
 if __name__ == "__main__":
@@ -64,9 +46,7 @@ if __name__ == "__main__":
     settings, base_params = get_experiment_config(expt_id)
     prob_settings = settings['probability_settings']
 
-    restricted_tech = bool(int(sys.argv[2]))
-
-    if len(sys.argv) > 3: scenario_id = int(sys.argv[3])
+    if len(sys.argv) > 2: scenario_id = int(sys.argv[2])
     else: scenario_id = None
 
     # ========================================
@@ -80,8 +60,7 @@ if __name__ == "__main__":
 
     design_wrapper = partial(
         posterior_design,
-        settings=settings,
-        restricted_tech=restricted_tech
+        settings=settings
     )
 
     scenarios_to_design = list(range(offset,n_scenarios_to_do+offset))
